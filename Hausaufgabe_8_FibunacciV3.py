c8a1e066b76038d25fe8165334598f8df822bc5c
5h3rr1ll <a@sherrill.de>
2017-02-09 11:31:48 +0100

    Fibonacci V3

---
+++ Hausaufgabe_8_FibunacciV3.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def Fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return Fibo(n-1) + Fibo(n-2)
+
+print(Fibo(int(input("Die Summe nach dem wievielten Wachstumszyklus möchtest du wissen? "))))
